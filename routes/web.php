c700a064465dc3547ad259fc86b734e96d4997af
ahmdsydysf <a.sayed.xc@gmail.com>
2025-11-20 17:17:04 +0400

    s

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Portfolio2Controller;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [Portfolio2Controller::class, 'index']);
 
 Route::get('/portfolio2', [Portfolio2Controller::class, 'index'])->name('portfolio2.index');
